56be310c7879d018145391b163268c9a2753cc84
Marco Biscaro <marco.biscaro@nubank.com.br>
2021-02-02 16:08:34 -0300

    Add test cases for cookMeat

--- flutter_app/test/nu_burguer_test.dart
+++ flutter_app/test/nu_burguer_test.dart
@@ -12,30 +12,32 @@ void main() {
 
   test(
       'processOrders should return a stream of burgers with one burger per order',
-      () {
-    final Stream<Order> orders = Stream.fromIterable([Order(), Order()]);
-    final Stream<Burger> burguer = processOrders(orders);
+          () {
+        final Stream<Order> orders = Stream.fromIterable([Order(), Order()]);
+        final Stream<Burger> burguer = processOrders(orders);
 
-    expectLater(burguer, emitsInOrder([isA<Burger>(), isA<Burger>()]));
-  });
+        expectLater(burguer, emitsInOrder([isA<Burger>(), isA<Burger>()]));
+      });
 
   test('processOrders should return a stream of burgers with different types',
-      () {
-    final Stream<Order> orders = Stream.fromIterable([
-      Order(
-          burgerMeatType: MeatType.future, burgerBreadType: BreadType.italian),
-      Order(burgerMeatType: MeatType.meat, burgerBreadType: BreadType.brioche),
-    ]);
+          () {
+        final Stream<Order> orders = Stream.fromIterable([
+          Order(
+              burgerMeatType: MeatType.future,
+              burgerBreadType: BreadType.italian),
+          Order(burgerMeatType: MeatType.meat,
+              burgerBreadType: BreadType.brioche),
+        ]);
 
-    final Stream<Burger> burgers = processOrders(orders);
+        final Stream<Burger> burgers = processOrders(orders);
 
-    expectLater(
-        burgers,
-        emitsInOrder([
-          Burger(meatType: MeatType.future, breadType: BreadType.italian),
-          Burger(meatType: MeatType.meat, breadType: BreadType.brioche),
-        ]));
-  });
+        expectLater(
+            burgers,
+            emitsInOrder([
+              Burger(meatType: MeatType.future, breadType: BreadType.italian),
+              Burger(meatType: MeatType.meat, breadType: BreadType.brioche),
+            ]));
+      });
 
   test('italian bread should take 2 seconds', () {
     fakeAsync((async) {
@@ -48,8 +50,42 @@ void main() {
   test('brioche bread should take 4 seconds', () {
     fakeAsync((async) {
       cutBread(burgerBreadType: BreadType.brioche)
+          .then(expectAsync1((_) {}, count: 0));
+      async.elapse(Duration(seconds: 3));
+    });
+
+    fakeAsync((async) {
+      cutBread(burgerBreadType: BreadType.brioche)
           .then(expectAsync1((_) {}, count: 1));
       async.elapse(Duration(seconds: 4));
+    });
+  });
+
+  test('future meat should take 60 seconds', () {
+    fakeAsync((async) {
+      cookMeat(MeatType.future)
+          .then(expectAsync1((_) {}, count: 0));
+      async.elapse(Duration(seconds: 20));
+    });
+
+    fakeAsync((async) {
+      cookMeat(MeatType.future)
+          .then(expectAsync1((_) {}, count: 1));
+      async.elapse(Duration(seconds: 60));
+    });
+  });
+
+  test('meat meat should take 90 seconds', () {
+    fakeAsync((async) {
+      cookMeat(MeatType.meat)
+          .then(expectAsync1((_) {}, count: 0));
+      async.elapse(Duration(seconds: 60));
+    });
+
+    fakeAsync((async) {
+      cookMeat(MeatType.meat)
+          .then(expectAsync1((_) {}, count: 1));
+      async.elapse(Duration(seconds: 90));
     });
   });
 }
