11410c2c1cd2553d3578610f33c6bd1d6f8ca7f9
Adson Leal <adson.leal@nubank.com.br>
2021-02-02 14:50:46 -0300

    add processOrders

--- flutter_app/test/nu_burguer_test.dart
+++ flutter_app/test/nu_burguer_test.dart
@@ -1,5 +1,10 @@
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  
+  test('processOrders should return a stream of', () {
+    final Stream<Order> orders = Stream.fromIterable([Order()]);
+    final Stream<Burger> burguer = processOrders(orders);
+
+    expectLater(burguer, emitsInOrder([isA<Burger>()]));
+  });
 }
